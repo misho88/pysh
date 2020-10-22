26f2cb032e138a74172591b37c1082b86f3d3d7b
Mihail Georgiev <misho88@gmail.com>
2020-10-22 19:31:57 -0400

    installation

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+import pysh
+long_description = pysh.__doc__
+
+setuptools.setup(
+    name='pysh',
+    version='0.0.1',
+    author='Mihail Georgiev',
+    author_email='misho88@gmail.com',
+    description='pysh - easy-to-use subprocess.run',
+    long_description=long_description,
+    long_description_content_type='text/plain',
+    url='https://github.com/misho88/pysh',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    py_modules=['pysh']
+)
