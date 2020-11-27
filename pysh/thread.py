9c0efe2dc6b527a8a79d2cd2ac912d7c43bca34a
Mihail Georgiev <misho88@gmail.com>
2020-11-27 13:49:26 -0500

    Thread can be used as context manager

--- pysh/thread.py
+++ pysh/thread.py
@@ -8,6 +8,12 @@ class Thread(threading.Thread):
 
     >>> Thread(lambda: 1 + 2).start().join()
     3
+    >>> with Thread(lambda: 1 + 2) as thread: thread.join()
+    ...
+    3
+    >>> with Thread(lambda: print('hello')) as thread: pass
+    ...
+    hello
     """
     def __init__(self, target):
         """initilialize the thread
@@ -37,3 +43,10 @@ class Thread(threading.Thread):
         if hasattr(func, 'func') and func.func is not func:
             return Thread.get_name(func.func)
         return repr(func)
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.join()
