bb2b5808e1aae84427f567d16effa469fce968df
Mihail Georgiev <misho88@gmail.com>
2021-01-09 18:47:24 -0500

    some basic fcntl support for FD class

--- pysh/fd.py
+++ pysh/fd.py
@@ -2,6 +2,68 @@ __all__ = 'FD',
 
 import os
 from errno import EBADF
+from functools import partial, cached_property
+
+try:
+    import fcntl
+except ImportError:
+    fcntl = None
+
+
+if fcntl is not None:
+    def get_cmd(cmd):
+        return getattr(fcntl, cmd.upper()) if isinstance(cmd, str) else cmd
+
+    def get_arg(arg):
+        return getattr(os, arg.upper()) if isinstance(arg, str) else arg
+
+    def make_property(arg):
+        arg = get_arg(arg)
+
+        def get(self):
+            return self.get(arg)
+
+        def set(self, value):
+            return self.set(arg, value)
+
+        return property(get).setter(set)
+
+    class Flags:
+        NAMES = (
+            'O_RDONLY', 'O_WRONLY', 'O_RDWR', 'O_ACCMODE', 'O_CREAT', 'O_EXCL',
+            'O_NOCTTY', 'O_TRUNC', 'O_APPEND', 'O_NONBLOCK', 'O_DSYNC', 'O_ASYNC',
+            'O_DIRECT', 'O_DIRECTORY', 'O_NOFOLLOW', 'O_NOATIME', 'O_CLOEXEC',
+            'O_SYNC', 'O_PATH', 'O_TMPFILE'
+        )
+
+        def __init__(self, fd):
+            self.fd = fd
+
+        @property
+        def mask(self):
+            return fcntl.fcntl(self.fd, fcntl.F_GETFL)
+
+        @mask.setter
+        def mask(self, value):
+            return fcntl.fcntl(self.fd, fcntl.F_SETFL, value)
+
+        def get(self, flag):
+            flag = get_arg(flag)
+            return bool(self.mask & flag)
+
+        def set(self, flag, value=1):
+            from fcntl import fcntl, F_GETFL, F_SETFL
+            if value not in (0, 1, False, True):
+                raise ValueError('value must be 0/1 or False/True')
+            flag = get_arg(flag)
+            mask = self.mask
+            mask = mask | flag if value else mask & ~flag
+            self.mask = mask
+
+        def unset(self, flag):
+            self.set(flag, 0)
+
+        locals().update((n, make_property(n)) for n in NAMES)
 
 
 class FD:
@@ -58,3 +120,15 @@ class FD:
 
     def __int__(self):
         return self.fd
+
+    if fcntl is not None:
+        def fcntl(self, cmd, arg=0):
+            if isinstance(cmd, str):
+                cmd = getattr(fcntl, cmd.upper())
+            if isinstance(arg, str):
+                arg = getattr(os, arg.upper())
+            return fcntl.fcntl(self.fd, cmd, arg)
+
+        @cached_property
+        def flags(self):
+            return Flags(self.fd)
